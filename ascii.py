823ba9086f8bd52f6fbcb77d5ba81aa60e127b12
Jp1200 <jprkr96@gmail.com>
2020-12-12 13:13:12 -0800

    ascii color

--- ascii.py
+++ ascii.py
@@ -3,33 +3,49 @@ import numpy as np
 import cv2
 
 class Converter:
-	def __init__(self, path='./Resources/lofi.jpg', font_size=6, color_lvl = 8):
+	def __init__(self, path='./Resources/lofi.jpg', font_size=10, color_lvl = 8):
 		pg.init()
 		self.path = path 
-		self.image = self.get_image()
+		self.image, self.gray_image = self.get_image()
 		self.COLOR_lvl = color_lvl
 		self.RES = self.WIDTH, self.HEIGHT = self.image.shape[0], self.image.shape[1]
 		self.surface = pg.display.set_mode(self.RES)
 		self.clock = pg.time.Clock()
-		self.ASCII_CHARS = '.",:;!~+-xmo*#W&8@'
+		self.ASCII_CHARS = ' ixzao*#MW&8%B@$'
 		self.ASCII_COEFF = 255 // (len(self.ASCII_CHARS)-1)
-		self.font = pg.font.SysFont('Courier',font_size, bold=True)
+		self.font = pg.font.SysFont('Courier',font_size, bold=False)
 		self.CHAR_step = int(font_size * 0.6)
-		self.RENDERED_ASCII_CHARS = [self.font.render(char, False, 'white') for char in self.ASCII_CHARS]
-
+		# self.RENDERED_ASCII_CHARS = [self.font.render(char, False, 'white') for char in self.ASCII_CHARS]
+		self.PALETTE, self.COLOR_COEFF = self.create_palette()
 
 	def draw_converted_image(self):
-		char_indices = self.image // self.ASCII_COEFF
+		char_indices = self.gray_image // self.ASCII_COEFF
+		color_indices = self.image // self.COLOR_COEFF
 		for x in range(0, self.WIDTH, self.CHAR_step):
 			for y in range(0, self.HEIGHT, self.CHAR_step):
 				char_index = char_indices[x,y]
 				if char_index:
-					self.surface.blit(self.RENDERED_ASCII_CHARS[char_index],(x,y))
+					char = self.ASCII_CHARS[char_index]
+					color = tuple(color_indices[x,y])
+					self.surface.blit(self.PALETTE[char][color],(x,y))
+	def create_palette(self):
+		colors, color_coeff = np.linspace(0,255, num=self.COLOR_lvl, dtype=int, retstep=True)
+		color_palette = [np.array([r,g,b]) for r in colors for g in colors for b in colors]
+		palette = dict.fromkeys(self.ASCII_CHARS, None)
+		color_coeff = int(color_coeff)
+		for char in palette:
+			char_palette = {}
+			for color in color_palette:
+				color_key = tuple(color // color_coeff)
+				char_palette[color_key] = self.font.render(char, False, tuple(color))
+			palette[char] = char_palette
+		return palette, color_coeff
 	def get_image(self):
 		self.cv2_image = cv2.imread(self.path)
 		transposed_image = cv2.transpose(self.cv2_image)
+		image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2RGB)
 		gray_image = cv2.cvtColor(transposed_image, cv2.COLOR_BGR2GRAY)
-		return gray_image
+		return image, gray_image
 	def draw_cv2_image(self):
 		resized_cv2_image = cv2.resize(self.cv2_image, (640, 360), interpolation=cv2.INTER_AREA)
 		cv2.imshow('img', resized_cv2_image)
