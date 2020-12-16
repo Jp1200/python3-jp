eb45790fd537fdd3d58589882ac304aa6107dae0
Jp1200 <jprkr96@gmail.com>
2020-12-15 18:11:29 -0800

    reaction time test loops through whole test and calulates average test time

--- reactions.py
+++ reactions.py
@@ -1,9 +1,7 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 URL = 'https://humanbenchmark.com/tests/reactiontime'
 PATH = '/Users/homepro/Development/CEG/chromedriver'
 
@@ -15,14 +13,23 @@ red = "rgb(206, 38, 54)"
 driver.get(URL)
 box_to_click = driver.find_element_by_css_selector(
     'div.css-saet2v.view-splash')
-# First Click
-box_to_click.click()
-print(box_to_click.value_of_css_property("background"))
-try:
-    wait = True
-    while wait:
-        if green in box_to_click.value_of_css_property("background"):
-            wait = False
-        print("red")
-finally:
+
+
+start = time.time()
+
+for i in range(5):
     box_to_click.click()
+    try:
+        wait = True
+        while wait:
+            if green in box_to_click.value_of_css_property("background"):
+                wait = False
+
+    finally:
+        print("Clicked!")
+        box_to_click.click()
+end = time.time()
+print(f"Average time to complete whole test ~{int(end-start)}")
+time.sleep(5)
+print("Closing...")
+driver.quit()
